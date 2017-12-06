c2781d616bf0f8268bd31ba4a173a96f05995cfb
Jim Wilenius <jim.wilenius@tomologic.com>
2017-12-06 10:10:21 +0100

    Added Day5 part 2

--- src/main/java/net/booru/adventofcode2017/Day5.java
+++ src/main/java/net/booru/adventofcode2017/Day5.java
@@ -10,6 +10,9 @@ class Day5
 
         int steps = escape(Util.readLinesAsIntegers("day5.in"));
         System.out.println("Steps: " + steps);
+
+        int steps2 = escape2(Util.readLinesAsIntegers("day5.in"));
+        System.out.println("Steps: " + steps2);
     }
 
     private static int escape(final int[] maze)
@@ -27,10 +30,27 @@ class Day5
         return steps;
     }
 
+    private static int escape2(final int[] maze)
+    {
+        int steps = 0;
+        int cursor = 0;
+        while (cursor < maze.length)
+        {
+            final int offset = maze[cursor];
+            maze[cursor] += offset >= 3 ? -1 : 1;
+            cursor = cursor + offset;
+            steps++;
+        }
+
+        return steps;
+    }
+
     private static void test()
     {
         Util.require(escape(new int[]{0,3,0,1,-3}), 5);
         Util.require(escape(new int[]{1,0,2,5,1}), 5);
+
+        Util.require(escape2(new int[]{0,3,0,1,-3}), 10);
 
         System.out.println("All tests ok");
     }
